dabade5e0546360fecae68a4a485bdcd5ca6cc16
泽嘉 李 <romanzli@outlook.com>
2018-03-26 14:33:10 +0800

    1

---
+++ Application.Quartz.NET.JobBase/UrlBaseJob.cs
@@ -0,0 +1,28 @@
+﻿using Quartz;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Net.Http;
+
+namespace Application.Quartz.NET.JobBase
+{
+    public class UrlBaseJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                HttpClient hc = new HttpClient();
+                HttpResponseMessage responsemessage =  await hc.GetAsync(context.JobDetail.JobDataMap["requestUrl"].ToString());
+                //写日志
+                //responsemessage.Content
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+    }
+}
