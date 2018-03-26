3e5282b96d0b85181504262f4e5f701d04874ac7
泽嘉 李 <romanzli@outlook.com>
2018-03-26 14:33:21 +0800

    2

---
+++ Application.Quartz.NET.Service/MyJobListener.cs
@@ -0,0 +1,55 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Application.Quartz.NET.Service
+{
+    /// <summary>
+    /// Job监听器
+    /// </summary>
+    public class MyJobListener : IJobListener
+    {
+        public string Name
+        {
+            get
+            {
+                return "Jobs_Listener";
+            }
+        }
+        /// <summary>
+        /// 一般不调用
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task JobExecutionVetoed(IJobExecutionContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// job执行前，调用
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task JobToBeExecuted(IJobExecutionContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// job执行完，调用
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="jobException"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task JobWasExecuted(IJobExecutionContext context, JobExecutionException jobException, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application.Quartz.NET.Service/Program.cs
@@ -0,0 +1,64 @@
+﻿using Quartz;
+using Quartz.Impl;
+using Quartz.Impl.Matchers;
+using Quartz.Logging;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+
+namespace Application.Quartz.NET.Service
+{
+    public class Program
+    {
+        //ILog log = LogProvider.GetLogger(typeof(Program));
+        private static void Main(string[] args)
+        {
+            RunProgram().GetAwaiter().GetResult();
+        }
+
+
+        private static async Task RunProgram()
+        {
+            var properties = new NameValueCollection();
+            properties["quartz.scheduler.instanceName"] = "RemoteServer";
+            properties["quartz.scheduler.exporter.type"] = "Quartz.Simpl.RemotingSchedulerExporter, Quartz";
+            //端口号
+            properties["quartz.scheduler.exporter.port"] = "555";
+            //名称
+            properties["quartz.scheduler.exporter.bindName"] = "QuartzScheduler";
+            //通道类型
+            properties["quartz.scheduler.exporter.channelType"] = "tcp";
+            properties["quartz.scheduler.exporter.channelName"] = "httpQuartz";
+            properties["quartz.scheduler.exporter.rejectRemoteRequests"] = "true";
+
+            properties["quartz.threadPool.type"] = "Quartz.Simpl.SimpleThreadPool, Quartz";
+            properties["quartz.threadPool.threadCount"] = "10";
+            properties["quartz.threadPool.threadPriority"] = "Normal";
+            //集群配置
+            properties["quartz.jobStore.clustered"] = "true";
+            //存储类型
+            properties["quartz.jobStore.type"] = "Quartz.Impl.AdoJobStore.JobStoreTX, Quartz";
+            //数据库驱动类型
+            properties["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.SqlServerDelegate, Quartz";
+            //表名前缀
+            properties["quartz.jobStore.tablePrefix"] = "QRTZ_";
+            //配置AdoJobStore以使用字符串作为JobDataMap值
+            properties["quartz.jobStore.useProperties"] = "true";
+            //
+            properties["quartz.jobStore.misfireThreshold"] = "60000";
+            //数据源名称
+            properties["quartz.jobStore.dataSource"] = "myDS";
+            //连接字符串
+            properties["quartz.dataSource.myDS.connectionString"] = @"Data Source=.;Initial Catalog=QuartzManager;User ID=sa;Password=123456";
+            //版本
+            properties["quartz.dataSource.myDS.provider"] = "SqlServer";
+            //集群中的每个节点必须具有唯一的instanceId
+            properties["quartz.scheduler.instanceId"] = "AUTO";
+            properties["quartz.serializer.type"] = "json";
+
+            ISchedulerFactory sf = new StdSchedulerFactory(properties);
+            IScheduler sched = await sf.GetScheduler();
+            sched.ListenerManager.AddJobListener(new MyJobListener(), GroupMatcher<JobKey>.AnyGroup());
+            await sched.Start();
+        }
+    }
+}
